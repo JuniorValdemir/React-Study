fb0a5955595aafcf8384154d2526eeffaafbd626
JVNJ1 <juniorvaldemir87@gmail.com>
2023-04-26 18:30:43 -0300

    Changes

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
-
+import Primeiro from './components/basics/Primeiro'
 
 ReactDOM.render(
-    <div><strong>Teste!!!!!!!!!!!!</strong></div>,
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
     document.getElementById('root')
     )
