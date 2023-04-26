7f686ed770bd386013826324c60afed9493595bf
JVNJ1 <juniorvaldemir87@gmail.com>
2023-04-26 18:27:15 -0300

    Changes

---
+++ src/components/basics/Primeiro.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function Primeiro() {
+    const msg = 'Sejam Bem Vindos!'
+    return (
+        <div>
+            <h1>First Test</h1>
+            <h2>{ msg }</h2>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import ReactDOM from 'react-dom'
 import React from 'react'
 import './index.css'
+import Primeiro from './components/basics/Primeiro.js'
+
 
 
 ReactDOM.render(
-    <div><strong>Teste!!!!!!!!!!!!</strong></div>,
+    <div>
+        <Primeiro></Primeiro>
+    </div>,
     document.getElementById('root')
     )
