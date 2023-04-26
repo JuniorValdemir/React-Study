67bc20910b12a31b86ac5a33ee3693dcf1f65906
JVNJ1 <juniorvaldemir87@gmail.com>
2023-04-26 12:32:04 -0300

    Atualização

--- src/index.js
+++ src/index.js
@@ -3,8 +3,7 @@ import React from 'react'
 import './index.css'
 
 
-const element = document.getElementById('root')
 ReactDOM.render(
     <div><strong>Teste!</strong></div>,
-    element
+    document.getElementById('root')
     )
